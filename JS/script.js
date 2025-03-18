cd345e724e7ec6071010b1d5048757682a4463ba
Rodrigues Anthony <anthony-rodrigues31@hotmail.fr>
2025-03-18 20:41:54 +0100

    commit 1

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+//creer les variables necessaires en recuperant les elements dans le DOM : 
+
+const champArticle = document.getElementById("champArticle");
+const ajoutArticle = document.getElementById("ajoutArticle");
+const articlesAjoutes = document.getElementById("articlesAjoutes");
+
+//Creer une fonction pour ajouter les elements dans la liste
+
+function ajoutElement(){
+    //creer les elements : 
+    let ajoutLi = document.createElement("li");
+    let bouttonSupprimer = document.createElement("button");
+    let texte = document.createElement("span");
+
+    //configurer le contenu des elements ajoutés :
+    
+}
